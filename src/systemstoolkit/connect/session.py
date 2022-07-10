7cc50cddf2ab3ce80933aca01b508898be7fdf00
Jonathan Olsten <jolsten@gmail.com>
2022-07-10 11:03:17 -0400

    add type hint for Connect object, using typing.TYPE_CHECKING to avoid circular import problem

--- src/systemstoolkit/connect/session.py
+++ src/systemstoolkit/connect/session.py
@@ -3,7 +3,7 @@ import socket
 import collections
 
 from systemstoolkit.exceptions import STKCommandError
-from systemstoolkit.connect.objects import Satellite
+from systemstoolkit.connect.objects import Scenario, Satellite
 from systemstoolkit.connect.objects.base import Object
 from systemstoolkit.connect import validators
 
@@ -23,13 +23,24 @@ class Connect:
         self,
         host: str = 'localhost',
         port: int = 5001,
+        log: bool = False,
     ) -> None:
         self.host = host
         self.port = port
+        self.log = log
         self._socket = None
-        
+        self._history = None
+    
+    def __enter__(self) -> None:
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb, sep="\n") -> None:
+        self.close()
+
     def close(self) -> None:
         self._socket.close()
+        self._history = None
     
     def connect(self) -> None:
         try:
@@ -38,13 +49,23 @@ class Connect:
                 socket.SOCK_STREAM,
             )
             self._socket.connect((self.host, self.port))
+            self._history = []
         except ConnectionRefusedError as msg:
             raise
     
     def send(self, command: str) -> None:
+        # Strip any trailing newlines
         command = command.rstrip()
+
+        # Send the string with one (required) newline
         self._socket.sendall(str.encode(command + '\n'))
+
+        # Check for ACK/NACK
         response = self._get_ack()
+        
+        if self.log:
+            self._history.append((command, response))
+        
         if response == 'NACK':
             raise STKCommandError(f'NACK: {command}')
         
@@ -62,18 +83,27 @@ class Connect:
     def get_single_message(self) -> SingleMessage:
         data = self._socket.recv(40)
         command_name, data_length = data.decode().split()
+        
+        # Determine length of message, get that many bytes
         data_length = int(data_length)
         data = self._socket.recv(data_length)
         message = data.decode()
+
         return SingleMessage(command_name, data_length, message)
 
     def get_multi_message(self) -> MultiMessage:
         data = self._socket.recv(40)
         command_name, data_length = data.decode().split()
+        
+        # Determine length of message, get that many bytes
         data_length = int(data_length)
         data = self._socket.recv(data_length)
+
+        # Determine the qty of SingleMessages, get them
         num_messages = int(data)
         messages = [self.get_single_message() for _ in range(num_messages)]
+        
+        # Get closing SingleMessage
         self.get_single_message()
         return MultiMessage(command_name, num_messages, messages)
 
@@ -82,25 +112,19 @@ class Connect:
         return [msg.Data for msg in multi_msg]
 
     def unload_all(self) -> None:
+        """Unload (delete) all objects including the current Scenario."""
         self.send('Unload / *')
 
-    def new_scenario(self, name: str) -> str:
+    def new_scenario(self, name: str) -> Scenario:
+        """Create a new Scenario (by first unloading all)."""
         self.unload_all()
-        self.send(f'New / Scenario {name}')
+        obj = Scenario(self, f'Scenario/{name}')
+        obj.create()
+        return obj
 
     def new_satellite(self, name: str) -> Satellite:
+        """Create a new Satellite with given name."""
         validators.name(name)
         obj = Satellite(self, f'*/Satellite/{name}')
         obj.create()
         return obj
-
-
-class _FakeConnect(Connect):
-    def close(self) -> None:
-        pass
-
-    def connect(self) -> None:
-        self._messages = []
-    
-    def send(self, command: str) -> None:
-        self._messages.append(command)
