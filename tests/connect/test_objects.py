1feec962ed915667d3a415322f4698deb98c8862
Jonathan Olsten <jolsten@gmail.com>
2022-07-14 00:23:07 -0400

    increased coverage of facility tests to 100%

--- tests/connect/test_objects.py
+++ tests/connect/test_objects.py
@@ -217,6 +217,20 @@ def test_create_facility():
             assert got == exp
 
 
+def test_facility_set_position_cartesian():
+    exp = 'SetPosition */Facility/AGIHQ Cartesian 1216360.0 -4736250.0 4081270.0'
+    with mock.patch('socket.socket') as mock_sock:
+        mock_sock.return_value.recv.return_value = b'ACK'
+
+        with Connect() as c:
+            print(c)
+            fac_obj = Facility(c, '*/Facility/AGIHQ')
+            fac_obj.set_position_cartesian('1216360.0', '-4736250.0', '4081270.0')
+
+            got = c._socket.sendall.call_args[0][0].decode().strip()
+            assert got == exp
+
+
 def test_facility_set_position_geodetic():
     exp = 'SetPosition */Facility/Wallops Geodetic 37.9 -75.5 0.0'
     with mock.patch('socket.socket') as mock_sock:
@@ -231,6 +245,20 @@ def test_facility_set_position_geodetic():
             assert got == exp
 
 
+def test_facility_set_position_geodetic_msl():
+    exp = 'SetPosition */Facility/Wallops Geodetic 37.9 -75.5 0.0 MSL'
+    with mock.patch('socket.socket') as mock_sock:
+        mock_sock.return_value.recv.return_value = b'ACK'
+
+        with Connect() as c:
+            print(c)
+            fac_obj = Facility(c, '*/Facility/Wallops')
+            fac_obj.set_position_geodetic('37.9', '-75.5', '0.0', msl=True)
+
+            got = c._socket.sendall.call_args[0][0].decode().strip()
+            assert got == exp
+
+
 def test_facility_set_position_geocentric():
     exp = 'SetPosition */Facility/Wallops Geocentric 37.9 -75.5 0.0'
     with mock.patch('socket.socket') as mock_sock:
@@ -240,6 +268,20 @@ def test_facility_set_position_geocentric():
             print(c)
             fac_obj = Facility(c, '*/Facility/Wallops')
             fac_obj.set_position_geocentric('37.9', '-75.5', '0.0')
+
+            got = c._socket.sendall.call_args[0][0].decode().strip()
+            assert got == exp
+
+
+def test_facility_set_position_geocentric_msl():
+    exp = 'SetPosition */Facility/Wallops Geocentric 37.9 -75.5 0.0 MSL'
+    with mock.patch('socket.socket') as mock_sock:
+        mock_sock.return_value.recv.return_value = b'ACK'
+
+        with Connect() as c:
+            print(c)
+            fac_obj = Facility(c, '*/Facility/Wallops')
+            fac_obj.set_position_geocentric('37.9', '-75.5', '0.0', msl=True)
 
             got = c._socket.sendall.call_args[0][0].decode().strip()
             assert got == exp
