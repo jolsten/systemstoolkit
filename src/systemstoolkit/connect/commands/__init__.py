2cdb7399c0ab3f42e420159e989d08bb875f1f3c
Jonathan Olsten <jolsten@gmail.com>
2022-07-19 13:13:02 -0400

    more command protocol

---
+++ src/systemstoolkit/connect/commands/__init__.py
@@ -0,0 +1,1 @@
+from .base import ConnectCommand, NewScenario, NewSatellite, NewFacility
--- src/systemstoolkit/connect/commands/base.py
+++ src/systemstoolkit/connect/commands/base.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractproperty
 from typing import Protocol, Optional
 import systemstoolkit.connect.validators as validators
 
@@ -7,42 +7,49 @@ class ConnectCommand(Protocol):
     command: str = ...
 
 
-class NewObject(ABC):
-    @abstractproperty
-    def cls(self) -> str:
-        pass
-
+class New:
     def __init__(
         self,
-        parent_path: str,
+        cls: str,
         name: str,
+        parent_path: Optional[str] = None,
         ignore: bool = False,
         default: bool = True,
         central_body: Optional[str] = None,
     ) -> None:
         validators.name(name)
         self.parent_path = parent_path
+        self.cls = cls
         self.name = name
         self.central_body = central_body
         self.ignore = ignore
         self.default = default
 
+        if self.parent_path is None and self.cls != 'Scenario':
+            raise ValueError(f'parent_path can only be None if cls=Scenario')
+
     @property
     def command(self) -> str:
+        if self.parent_path is None:
+            return f'New / {self.cls} {self.name}'
         return f'New / {self.parent_path}/{self.cls} {self.name}'
 
 
-class NewScenario(NewObject):
+class Unload:
+    """Unload <ApplicationPath> <ObjectPath> [RemAssignedObjs]"""
+    def __init__(
+        self,
+        path: str,
+        assigned_objects: bool = False,
+    ) -> None:
+        self.path = path
+        self.assigned_objects = assigned_objects
+
     @property
     def command(self) -> str:
-        return f'New / Scenario {self.name}'
+        return f'Unload / {self.path}' + ' RemAssignedObjects' if self.assigned_objects else ''
 
 
-class NewSatellite(NewObject):
-    cls = 'Satellite'
 
-
-class NewFacility(NewObject):
-    cls = 'Facility'
 
 
